64f4ecb60ae3b5997eb27ba1f4d113808a486b56
lola <lola.tosetto@etu.univ-grenoble-alpes.fr>
2024-03-04 15:33:39 +0100

    Exo 2

---
+++ server/src/main/java/fr/uga/l3miage/tp1/exo2/models/Session.java
@@ -0,0 +1,18 @@
+package fr.uga.l3miage.tp1.exo2.models;
+
+import javax.persistence.*;
+import java.util.UUID;
+
+@Entity
+@Table(name="session")
+public class Session {
+    @Id
+    @Column(name="id_session")
+    private UUID idSession;
+    @Column(name="last_command")
+    private String lastCommand;
+    @Column(name="lock")
+    private boolean lock;
+    @OneToOne(mappedBy ="session")
+    private User user;
+}
