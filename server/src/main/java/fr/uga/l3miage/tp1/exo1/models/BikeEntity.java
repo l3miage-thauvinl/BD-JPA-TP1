a5f56baee368092c5168b1ae882e8f96b9300099
lola <lola.tosetto@etu.univ-grenoble-alpes.fr>
2024-03-25 07:45:33 +0100

    correction exo 1

--- server/src/main/java/fr/uga/l3miage/tp1/exo1/models/BikeEntity.java
+++ server/src/main/java/fr/uga/l3miage/tp1/exo1/models/BikeEntity.java
@@ -6,23 +6,15 @@ import fr.uga.l3miage.tp1.exo1.enums.ShifterTypeClass;
 import javax.persistence.*;
 import java.time.LocalDate;
 @Entity
-@Table(name="bike_ent")
 public class BikeEntity {
     @Id
-    @Column(name="immat", length=8)
     private String immatriculation;
-    @Column(name="capacity")
     private Integer capacity;
-    @Column(name="CylinderNumber")
     @Enumerated(EnumType.ORDINAL)
     private CylinderNumberClass.CylinderNumber cylinderNumber;
-    @Column(name="automatic")
     private boolean automatic;
-    @Column(name="shifter")
     private boolean shifter;
-    @Column(name="schifterType")
     @Enumerated(EnumType.STRING)
     private ShifterTypeClass.ShifterType shifterType;
-    @Column(name="circulationDate")
     private LocalDate circulationDate;
 }
